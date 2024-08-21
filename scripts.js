2acf8a4e4de944a6e82fc2ea19958b46977eac40
Kauê <kauetmurakami@gmail.com>
2024-08-21 18:03:31 -0300

    prevent add empty values

--- scripts.js
+++ scripts.js
@@ -16,13 +16,26 @@ const handlingForms = {
 	data() {
 		return {
 			members: window.members,
-			newMember: { } //com v-model ele cria as propriedades
+			newMember: {} //com v-model ele cria as propriedades
 		}
 	},
-	methods:{
-		addMember : function(){
-			this.members.push(this.newMember)
-			this.newMember = {}
+	methods: {
+		addMember: function () {
+			//Nós só conseguimos que os dados inseridos no formulário 
+			//sejam enviados para as propriedades de dado 
+			//isso é One way Data Binding ou seja os dados 
+			//só fluem nessa direção
+
+			//Caso quisermos que qualquer alteração na propriedade de dados
+			//seja enviada de volta para o formulário de usuário 
+			// então temos ums Two way Data Binding
+			if (this.newMember.fname && this.newMember.lname && this.newMember.instrument) {
+				this.members.push(this.newMember)
+				this.newMember = {}
+			}else {
+				alert('All fields must be filled')
+			}
+
 		}
 	}
 
